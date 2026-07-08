9d759d22bf4e45d0092cf9553e18760df88b0de8
khoadangbui007-png <khoadangbui007@gmail.com>
2026-07-08 14:18:46 +0700

    Delete Buổi 3_BuiDangKhoa_2574802010947 directory

--- Buổi 3_BuiDangKhoa_2574802010947/bai1.py
+++
@@ -1,9 +0,0 @@
-def one_pass_bubble_sort(a):
-    n = len(a)
-    for i in range(n - 1):
-        if a[i] > a[i + 1]:
-            a[i], a[i + 1] = a[i + 1], a[i]
-    return a
-
-a = [5, 1, 4, 2, 8]
-print(one_pass_bubble_sort(a))  
